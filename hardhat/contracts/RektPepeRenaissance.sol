62807a7a9d05efb89dfc7ebbf6d26da0b4e387e1
ryantinder <ryantinder56@gmail.com>
2022-05-25 21:14:57 -0500

    Basis for unit tests

--- hardhat/contracts/RektPepeRenaissance.sol
+++ hardhat/contracts/RektPepeRenaissance.sol
@@ -2,12 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RektPepeRenaissance is ERC721A {
-    constructor() ERC721A("RektPepeRenaissance", "RPR") {
+contract RektPepeRenaissance is ERC721A, Ownable {
+    event Mint(address indexed to, uint256 indexed quantity);
+
+    constructor() ERC721A("RektPepeRenaissance", "RPR") Ownable() {
 
     }
-    function mint(address to) public {
-        _mint(to, 1);
+    function mint(address to, uint256 quantity) public onlyOwner {
+        _mint(to, quantity);
+        emit Mint(to, quantity);
     }
 }
--- hardhat/test/rpr.spec.ts
+++ hardhat/test/rpr.spec.ts
@@ -12,25 +12,95 @@ describe.only("RPR Tests", async () => {
     let RPR: RektPepeRenaissance
     let RPR_Factory: RektPepeRenaissance__factory
     let owner: SignerWithAddress;
+    let actorA: SignerWithAddress;
     //let actorA: SignerWithAddress;
     before(async () => {
         const signers = await ethers.getSigners();
         owner = signers[0];
         RPR_Factory = await ethers.getContractFactory("RektPepeRenaissance") as RektPepeRenaissance__factory
+
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: ["0xbcf5ab858cb0c003adb5226bdbfecd0bfd7b6d9f"]
+          });
+        actorA = await ethers.getSigner("0xbcf5ab858cb0c003adb5226bdbfecd0bfd7b6d9f")
     })
     const deploy = async () => {
         RPR = await RPR_Factory.deploy()
         await RPR.deployed();
         console.log(`\tContract deployed to: ${RPR.address}`);
     }   
-    it("deploy contract", async () => {
-        await deploy();
+    describe("Minting/Burning", async () => {
+        beforeEach(async () => {
+            await deploy();
+        })
+        it("Mint 1 NFT", async () => {
+            // Pre conditions
+            console.log(`\tAddress: ${actorA.address}`);
+            console.log(`\tPre balance: ${await RPR.balanceOf(actorA.address)}`);
+
+            // Actions
+            await expect(
+                RPR.connect(owner).mint(actorA.address, 1)
+            )
+            .to.emit(RPR, "Mint")
+            .to.emit(RPR, "Transfer")
+            
+            // What you expect it to look like afterwards
+            console.log(`\tPost balance: ${await RPR.balanceOf(actorA.address)}`);
+            console.log(`\tOwner of id 1: ${await RPR.ownerOf(0)}`);
+
+            expect( 
+                await RPR.ownerOf(1) == actorA.address
+            )
+        })
+        it("Batch mint 2 NFTs", async () => {
+            
+        })
+        it("Batch mint 5 NFTs", async () => {
+            
+        })
+        it("burn 1 NFT", async () => {
+            
+        })
+        it("Batch burn 5 NFTs", async () => {
+            
+        })
     })
-    it("Mint one NFT", async () => {
-        console.log(`owner of: ${await RPR.balanceOf(owner.address)}`);
-        const tx = await RPR.mint(owner.address);
-        tx.wait();
-        console.log(`owner of: ${await RPR.balanceOf(owner.address)}`);
-        console.log(`address of: ${await RPR.ownerOf(0)}`);
+    describe("Transfer", async () => {
+        it("Transfer single", async () => {
+            
+        })
+        it("Transfer multiple NFTs", async () => {
+            
+        })
+        it("Transfer via allowance", async () => {
+            
+        })
+        it("Batch transfer via allowance", async () => {
+            
+        })
+    })
+    it("Transfer contract ownership", async () => {
+        // RPR Renounce ownership
+        
+    })
+    it("Withdraw deposited funds to address", async () => {
+        
+    })
+    // What API for this? Seaport?
+    describe("List on OpenSea", async () => {
+        it("Set up opensea API", async () => {
+            
+        })
+        it("List on opensea?", async () => {
+            
+        })
+    })
+    // Do last
+    describe("Smart Wallet suite", async () => {
+        it("I don't know what he wants here.", async () => {
+            
+        })
     })
 })
